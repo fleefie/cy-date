2e5be26ce679ec25a25b3ce031479508a9533ab8
fleefie <waally2005@tuta.io>
2024-05-02 14:29:28 +0200

    Setup login

--- login.php
+++ login.php
@@ -1,18 +1,25 @@
 <?php
-
 session_start();
-
-
-if (!isset($_SESSION['loggedin'])) {
-
-    header('Location: verification.php'); 
-
+if (isset($_SESSION['loggedin'])) {
+    header('Location: main.php'); 
     exit;
-
-} else {
-
-    echo "Bonjour, ".$_SESSION['username'].". Vous êtes connecté.<br>";
-
 }
 
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $users = array(
+        'KHEFY' => 'Charles',
+        'Camelia' => 'Leclerc'
+  );
+
+    if (array_key_exists($username, $users) && $users[$username] == $password) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        header('Location: main.php');
+        exit;
+    } else {
+       header("Location: index.html");
+    }
+}
 ?>
