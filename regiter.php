4d2d2b2b680228d7d5b6eaa10d69760ae0d48d4d
KHEFY <133869969+KHEFY@users.noreply.github.com>
2024-05-14 19:14:31 +0200

    Update register.php

--- regiter.php
+++ regiter.php
@@ -1,37 +1,59 @@
 <?php
 session_start();
-if (isset($_SESSION['loggedin'])) {
-    header('Location: main.php'); 
+if (isset($_SESSION['username'])) {
+    header('Location: login.php'); 
     exit;
 }
+if($_SERVER["REQUEST_METHOD"]=="POST") {
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $users = array(
-        'KHEFY' => 'Charles',
-        'Camelia' => 'Leclerc'
-  );
+    if(empty(trim($_POST["username"])) || empty(trim($_POST["password"])) || empty(trim($_POST["description"]))) {
+        $error = "All fields are required";
+    }
+    else {
+        $username = trim($_POST["username"]);
+        $password = trim($_POST["password"]);
+        $description = trim($_POST["description"]);
 
-    if (array_key_exists($username, $users) && $users[$username] == $password) {
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $username;
-        header('Location: main.php');
-        exit;
-    } else {
-       header("Location: register.php");
+        if(file_exists("users/" . $username . ".txt")){
+            $error = "Username already exists. Please choose another one.";
+        }
+        else {
+            $userID = uniqid();
+            $userData = $userID . "|" . $username . "|" . password_hash($password,PASSWORD_DEFAULT) . "|" . $description . PHP_EOL;
+
+            file_put_contents("users/" . $username . ".txt" , $userData, FILE_APPEND);
+
+            header("Location:login.php");
+            exit;
+        }
     }
 }
 ?>
 
-
-
-
-
-
-
-
-
-
-
->
+<!DOCTYPE html> 
+<html>
+<head>
+    <title>Inscription</title>
+</head> 
+<body> 
+    <h2>Inscription</h2> 
+    <form action="" method="post">
+        <div>
+            <label for="username"> Username :</label>
+            <input type="text" name="username" id="username">
+        </div> 
+        <div>
+            <label for="password"> Password :</label> 
+            <input type="password" name="password" id="password"> 
+        </div> 
+        <div> 
+            <label for="description"> Description :</label> 
+            <textarea name="description" id="description" rows="3"></textarea>
+        </div> 
+        <div> 
+            <input type="submit" value="Register">
+        </div> 
+    </form> 
+    <p> Got an account ? <a href="login.php"> Login here</a> </p> 
+</body> 
+</html>
